31c76349381b73ab40fc3b86009a2e6bad478a97
Виталий <vpvedineev@edu.hse.ru>
2019-05-15 15:26:43 +0300

    init

--- src/main/java/mysystem/app/ScrumSystemApplication.java
+++ src/main/java/mysystem/app/ScrumSystemApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ScrumSystemApplication {
 
-
+//main
     public static void main(String... args) {
         SpringApplication.run(ScrumSystemApplication.class, args);
     }
